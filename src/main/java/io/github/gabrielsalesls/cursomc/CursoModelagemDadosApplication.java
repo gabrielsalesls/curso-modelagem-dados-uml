fcd931436032fab60a9b2f916350dd0f14a4f88f
Gabriel Sales <salesgabriel311@gmail.com>
2020-08-27 17:33:35 -0300

    Projeto Criado

---
+++ src/main/java/io/github/gabrielsalesls/cursomc/CursoModelagemDadosApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabrielsalesls.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoModelagemDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoModelagemDadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gabrielsalesls/cursomc/CursoModelagemDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielsalesls.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoModelagemDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
