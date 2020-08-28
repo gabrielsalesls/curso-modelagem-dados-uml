6d4eb7b28e8421a57d6c5bdd09206aa0c7526de0
Gabriel Sales <salesgabriel311@gmail.com>
2020-08-28 12:34:41 -0300

    Criando operacao de instanciacao

--- src/main/java/io/github/gabrielsalesls/cursomc/CursoModelagemDadosApplication.java
+++ src/main/java/io/github/gabrielsalesls/cursomc/CursoModelagemDadosApplication.java
@@ -1,13 +1,32 @@
 package io.github.gabrielsalesls.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.gabrielsalesls.cursomc.domain.Categoria;
+import io.github.gabrielsalesls.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoModelagemDadosApplication {
+public class CursoModelagemDadosApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemDadosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
