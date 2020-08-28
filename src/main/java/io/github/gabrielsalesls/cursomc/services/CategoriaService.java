b6739d06654f1fc09be555d85ea152abd15580f8
Gabriel Sales <salesgabriel311@gmail.com>
2020-08-28 16:43:05 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/io/github/gabrielsalesls/cursomc/services/CategoriaService.java
+++ src/main/java/io/github/gabrielsalesls/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import io.github.gabrielsalesls.cursomc.domain.Categoria;
 import io.github.gabrielsalesls.cursomc.repositories.CategoriaRepository;
+import io.github.gabrielsalesls.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,10 +15,12 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) throws ObjectNotFoundException {
 		Optional<Categoria> obj = repo.findById(id);
 		
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id 
+				+ ", Tipo: " + Categoria.class.getName()));
 	}
 	
 }
