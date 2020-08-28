a21e24cc958d61df6973c04d6d4f44c99e79473e
Gabriel Sales <salesgabriel311@gmail.com>
2020-08-28 11:12:01 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/io/github/gabrielsalesls/cursomc/resources/CategoriaResource.java
+++ src/main/java/io/github/gabrielsalesls/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package io.github.gabrielsalesls.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.gabrielsalesls.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 	
 }
